27009b3b34db543130ff79870ade37f91e20bace
Ореолов Евгений <evoreo@yandex.ru>
2023-02-13 20:13:43 +0300

    JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void onTheBorder() {
+        int amount = 999;
+        int expected = 1;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void border() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void afterTheBorder() {
+        int amount = 1001;
+        int expected = 999;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+}
